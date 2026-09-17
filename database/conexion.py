52795a105c2758d0ed5c64deb083d6cedec33a57
soffiale <martesporlatarde770@gmail.com>
2026-09-17 20:30:21 -0300

    Conectar conexion.py a variables de entorno

--- database/conexion.py
+++ database/conexion.py
@@ -1,13 +1,14 @@
 import mysql.connector
+from config import Config
 
 def obtener_conexion():
     try:
         conexion = mysql.connector.connect(
-            host="127.0.0.1",
-            port=3307,  # ← Debe ser 3307
-            user="root",
-            password="Arcangeles369",  # Deja la contraseña que utilices en tu XAMPP
-            database="rv_cargo"
+            host=Config.DB_HOST,
+            port=Config.DB_PORT,
+            user=Config.DB_USER,
+            password=Config.DB_PASSWORD,
+            database=Config.DB_NAME
         )
         return conexion
     except Exception as e:
